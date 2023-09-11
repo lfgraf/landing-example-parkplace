cfdfd91bf0f1b0f3eb823fe58177de54d25d9102
Raffaele Vitale <raffaelevitalebusiness@gmail.com>
2023-09-11 17:53:18 +0100

    first

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 
 const HeroSection = () => {
@@ -6,7 +8,7 @@ const HeroSection = () => {
       <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
         <div className="text-center lg:w-2/3 w-full">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Microdosing synth tattooed vexillologist
+            Text
           </h1>
           <p className="mb-8 leading-relaxed">
             Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
